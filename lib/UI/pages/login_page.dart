83bc91c8d7c8bc0aea55923e3f4872e5ffb35bae
João <joaocdvancea@gmail.com>
2024-10-09 22:04:32 -0300

    tela login 90% concluida

--- lib/UI/pages/login_page.dart
+++ lib/UI/pages/login_page.dart
@@ -1,92 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
-  bool _isObscured = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 150),
-            Text('Login',
-                style: GoogleFonts.montserrat(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black)),
-            const SizedBox(height: 50),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
-                'Login',
-                style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 22,
-                    color: Colors.grey),
-              ),
-              const TextField(
-                decoration: InputDecoration(
-                  hintText: 'Login',
-                  border: UnderlineInputBorder(),
-                ),
-              ),
-            ]),
-            const SizedBox(height: 20),
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
-                'Senha',
-                style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 22,
-                    color: Colors.grey),
-              ),
-              PasswordField()
-            ]),
-            const SizedBox(height: 80),
-            Center(
-              child: Container(
-                width: double.infinity,
-                height: 72,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Color(0xFF4960F9), Color(0xFF1433FF)],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SvgPicture.asset('login.svg', width: 400, height: 400),
+          const SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Login',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                const SizedBox(height: 50),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    'Login',
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 22,
+                        color: Colors.grey),
                   ),
-                  borderRadius: BorderRadius.circular(28),
-                ),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.transparent,
-                    shadowColor: Colors.transparent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Login',
+                      border: UnderlineInputBorder(),
                     ),
                   ),
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const SizedBox(width: 20),
-                      Text(
-                        'Login',
-                        style: GoogleFonts.montserrat(
-                          fontSize: 20,
-                          color: Colors.white
-                        ),
+                ]),
+                const SizedBox(height: 20),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    'Senha',
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 22,
+                        color: Colors.grey),
+                  ),
+                  PasswordField()
+                ]),
+                const SizedBox(height: 80),
+                Center(
+                  child: Container(
+                    width: double.infinity,
+                    height: 72,
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Color(0xFF4960F9), Color(0xFF1433FF)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
                       ),
-                      const SizedBox(width: 160),
-                      const Icon(Icons.arrow_forward, color: Colors.white),
-                    ],
+                      borderRadius: BorderRadius.circular(28),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.centerRight,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.transparent,
+                            shadowColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 5, 20, 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Login',
+                                  style: GoogleFonts.montserrat(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                                const Icon(Icons.arrow_forward, color: Colors.white),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: -10,
+                          right: 0,
+                          child: SvgPicture.asset('purpleMoon.svg', height: 50, width: 85))
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

fa0a42a4b98b70d6faefd17354f7a592f7a3254c
João <joaocdvancea@gmail.com>
2024-10-17 20:29:02 -0300

    tela gerenciar turmas concluída

--- lib/UI/pages/manage_classes.dart
+++ lib/UI/pages/manage_classes.dart
@@ -1,3 +1,5 @@
+import 'package:copa/UI/pages/edit_class.dart';
+import 'package:copa/UI/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -74,7 +76,12 @@ class _ManageClassesState extends State<ManageClasses> {
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(28)),
                               minimumSize: const Size(151, 56)),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const EditClass()));
+                          },
                           child: Text(
                             'Editar Turma',
                             style: GoogleFonts.montserrat(
@@ -88,109 +95,179 @@ class _ManageClassesState extends State<ManageClasses> {
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 280, 20, 20),
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: const Color(0xFFDFDFDF),
-                  borderRadius: BorderRadius.circular(40)),
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      DropdownButtonFormField<String>(
-                        value: selectedClass,
-                        items: classes.map((String className) {
-                          return DropdownMenuItem<String>(
-                            value: className,
-                            child: Text(className),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedClass = newValue;
-                          });
-                        },
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey[200],
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(40),
-                            border: Border.all(color: const Color(0xFFF2F4F8))),
-                        child: TextField(
-                          decoration: InputDecoration(
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFDFDFDF),
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          DropdownButtonFormField<String>(
+                            value: selectedClass,
+                            items: classes.map((String className) {
+                              return DropdownMenuItem<String>(
+                                value: className,
+                                child: Text(className),
+                              );
+                            }).toList(),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedClass = newValue;
+                              });
+                            },
+                            decoration: InputDecoration(
                               filled: true,
-                              fillColor: Colors.transparent,
-                              border: InputBorder.none,
-                              hintText: 'Presença',
-                              hintStyle: GoogleFonts.montserrat(
-                                  color: const Color(0xFF979797))),
-                        ),
-                      ),
-                      const SizedBox(height: 30),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(40),
-                            border: Border.all(color: const Color(0xFFF2F4F8))),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.transparent,
-                              border: InputBorder.none,
-                              hintText: 'Evento',
-                              hintStyle: GoogleFonts.montserrat(
-                                  color: const Color(0xFF979797))),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                            colors: [Color(0xFF4960F9), Color(0xFF1433FF)],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          ),
-                          borderRadius: BorderRadius.circular(28),
-                        ),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.transparent,
-                            shadowColor: Colors.transparent,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
+                              fillColor: Colors.grey[200],
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none,
+                              ),
                             ),
                           ),
-                          onPressed: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Atribuir Pontos',
-                                  style: GoogleFonts.montserrat(
-                                      fontSize: 15, color: Colors.white),
+                          const SizedBox(height: 30),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(40),
+                                border:
+                                    Border.all(color: const Color(0xFFF2F4F8))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.transparent,
+                                  border: InputBorder.none,
+                                  hintText: 'Presença',
+                                  hintStyle: GoogleFonts.montserrat(
+                                      color: const Color(0xFF979797))),
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(40),
+                                border:
+                                    Border.all(color: const Color(0xFFF2F4F8))),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.transparent,
+                                  border: InputBorder.none,
+                                  hintText: 'Evento',
+                                  hintStyle: GoogleFonts.montserrat(
+                                      color: const Color(0xFF979797))),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                colors: [Color(0xFF4960F9), Color(0xFF1433FF)],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.circular(28),
+                            ),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.transparent,
+                                shadowColor: Colors.transparent,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
                                 ),
-                                const Icon(Icons.chevron_right,
-                                    color: Colors.white),
-                              ],
+                              ),
+                              onPressed: () {},
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Atribuir Pontos',
+                                      style: GoogleFonts.montserrat(
+                                          fontSize: 15, color: Colors.white),
+                                    ),
+                                    const Icon(Icons.chevron_right,
+                                        color: Colors.white),
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
+                        ]),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Column(
+                  //Representantes column
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: Color(0xFF2B47FC), width: 1))),
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Representantes',
+                                style: GoogleFonts.montserrat(fontSize: 15)),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                              },
+                              child: Container(
+                                width: 30,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                        color: const Color(0xFF2743FD), width: 2),
+                                    color: Colors.transparent),
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Color(0xFF2743FD),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                    ]),
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  //Ocorrências column
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: Color(0xFF2B47FC), width: 1))),
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Ocorrências',
+                                style: GoogleFonts.montserrat(fontSize: 15)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           )
         ])

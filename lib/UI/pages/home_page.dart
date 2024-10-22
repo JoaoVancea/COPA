3ab747c769428eaf1c12fd11d0693f7b880afaed
João <joaocdvancea@gmail.com>
2024-10-22 17:45:26 -0300

    mostrando o pódigo na tela de gráficos

--- lib/UI/pages/home_page.dart
+++ lib/UI/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:copa/UI/pages/events_page.dart';
 import 'package:copa/UI/pages/manage_classes.dart';
 import 'package:copa/UI/pages/profile_page.dart';
@@ -66,8 +67,72 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
 
-class HomeContent extends StatelessWidget {
+class _HomeContentState extends State<HomeContent> {
+  Map<String, int> turmasPontuacao = {};
+  Map<String, String> turmasSigla = {}; // Mapa de turmaId para sigla
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  // Função para buscar os dados das turmas e eventos
+  Future<void> _fetchData() async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    // Buscar turmas
+    QuerySnapshot turmasSnapshot = await firestore.collection('turmas').get();
+    Map<String, String> siglas = {};
+
+    // Mapear o ID da turma para a sua sigla
+    for (var doc in turmasSnapshot.docs) {
+      var data = doc.data() as Map<String, dynamic>;
+      String turmaId = doc.id;
+      String sigla = data['sigla'] ?? '';
+      siglas[turmaId] = sigla;
+    }
+
+    // Buscar eventos
+    QuerySnapshot eventosSnapshot = await firestore.collection('evento').get();
+    Map<String, int> somaPontuacao = {};
+
+    // Iterar sobre cada documento da coleção 'evento'
+    for (var doc in eventosSnapshot.docs) {
+      var data = doc.data() as Map<String, dynamic>;
+
+      String turmaId = data['turmaId'];
+      int valor = data['valor'];
+
+      // Somar valores apenas se forem positivos
+      if (valor > 0) {
+        if (somaPontuacao.containsKey(turmaId)) {
+          somaPontuacao[turmaId] = somaPontuacao[turmaId]! + valor;
+        } else {
+          somaPontuacao[turmaId] = valor;
+        }
+      }
+    }
+
+    // Certificar-se de que todas as turmas estão presentes no mapa de pontuações (mesmo com 0 pontos)
+    siglas.forEach((turmaId, sigla) {
+      if (!somaPontuacao.containsKey(turmaId)) {
+        somaPontuacao[turmaId] = 0;
+      }
+    });
+
+    // Atualizar o estado com as siglas e pontuações
+    setState(() {
+      turmasSigla = siglas; // Mapear ID -> Sigla
+      turmasPontuacao = somaPontuacao; // Mapear ID -> Pontuação
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -95,14 +160,17 @@ class HomeContent extends StatelessWidget {
                         children: [
                           Padding(
                             padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                            child: SvgPicture.asset('logoCopa.svg'),
+                            child: Text("Logo"), // Placeholder da logo
                           ),
-                          Image.asset('luko.png')
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundImage: AssetImage('luko.png'),
+                          ),
                         ],
                       ),
                       Text(
                         'Bem vindo!',
-                        style: GoogleFonts.montserrat(
+                        style: TextStyle(
                           fontSize: 24,
                           color: Colors.white,
                         ),
@@ -122,9 +190,46 @@ class HomeContent extends StatelessWidget {
                         color: const Color(0xFFDFDFDF),
                         borderRadius: BorderRadius.circular(40),
                       ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Column(),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Pontuação Geral',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                // Exibir o pódio baseado nas pontuações
+                                if (turmasPontuacao.isNotEmpty) ...[
+                                  _buildPodiumColumn(
+                                      '2º',
+                                      _getSiglaTurma(turmasPontuacao, 2),
+                                      _getValorTurma(turmasPontuacao, 2),
+                                      2,
+                                      const Color(0xFFB52FF8)), // Segundo lugar - azul
+                                  _buildPodiumColumn(
+                                      '1º',
+                                      _getSiglaTurma(turmasPontuacao, 1),
+                                      _getValorTurma(turmasPontuacao, 1),
+                                      1,
+                                      const Color(0xFF2B47FC)), // Primeiro lugar - dourado
+                                  _buildPodiumColumn(
+                                      '3º',
+                                      _getSiglaTurma(turmasPontuacao, 3),
+                                      _getValorTurma(turmasPontuacao, 3),
+                                      3,
+                                      const Color(0xFFB52FF8)), // Terceiro lugar - azul
+                                ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
@@ -135,5 +240,84 @@ class HomeContent extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  // Função para construir as colunas do pódio
+  Widget _buildPodiumColumn(
+      String posicao, String siglaTurma, int valor, int posicaoInt, Color color) {
+    double altura;
+    switch (posicaoInt) {
+      case 1:
+        altura = 150;
+        break;
+      case 2:
+        altura = 120;
+        break;
+      case 3:
+        altura = 100;
+        break;
+      default:
+        altura = 100;
+    }
+    return Column(
+      children: [
+        Text(
+          posicao, // Exibe a posição (1º, 2º, 3º)
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          height: altura,
+          width: 80,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Text(
+              valor.toString(),
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          siglaTurma,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  // Função para buscar a sigla da turma baseado na posição
+  String _getSiglaTurma(Map<String, int> turmas, int posicao) {
+    List<MapEntry<String, int>> sortedTurmas =
+        turmas.entries.toList()..sort((a, b) => b.value.compareTo(a.value));
+    String turmaId = sortedTurmas.length >= posicao
+        ? sortedTurmas[posicao - 1].key
+        : "N/A";
+
+    // Retornar a sigla correspondente ao turmaId
+    return turmasSigla.containsKey(turmaId) ? turmasSigla[turmaId]! : "N/A";
+  }
+
+  // Função para buscar o valor da turma baseado na posição
+  int _getValorTurma(Map<String, int> turmas, int posicao) {
+    List<MapEntry<String, int>> sortedTurmas =
+        turmas.entries.toList()..sort((a, b) => b.value.compareTo(a.value));
+    return sortedTurmas.length >= posicao
+        ? sortedTurmas[posicao - 1].value
+        : 0;
   }
 }
